38064057564d52eecfec3102a017cb944fcb780d
nico.mayer <NqhH32SRYSU2pQ>
2023-04-06 12:19:14 +0200

    .env settings

--- main.go
+++ main.go
@@ -4,24 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/nico-mayer/go-api/controllers"
 	"github.com/nico-mayer/go-api/models"
 )
-
-var port = 8080
 
 func main() {
 	models.People = append(models.People, models.Person{ID: "1", FirstName: "John", LastName: "Doe", Age: 30})
 	models.People = append(models.People, models.Person{ID: "2", FirstName: "Jane", LastName: "Doe", Age: 28})
 
-	handleRequests()
+	var port string
+	err := godotenv.Load()
+	if err != nil {
+		port = ":8080"
+	}
+	port = ":" + os.Getenv("PORT")
+
+	handleRequests(port)
 }
 
-func handleRequests() {
+func handleRequests(port string) {
 	http.HandleFunc("/people", controllers.GetPeople)
 	http.HandleFunc("/people/create", controllers.CreatePerson)
 	fmt.Println("Server listening on PORT: ", port)
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+	log.Fatal(http.ListenAndServe(port, nil))
 }
